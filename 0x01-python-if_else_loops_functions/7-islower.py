de1a103a14d878a97f8ccdccc142e451501aeadb
T-redd01 <tyrilreddy1@gmail.com>
2023-08-08 22:33:33 +0200

    forget to remove dbug code

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
     if ord(c) >= 97 and ord(c) <= 122:
-        print(ord(c))
         return True
     return False
-
-
-print("H => {}".format("lower" if islower("H") else "upper"))
-
-print("'4' => {}".format("lower" if islower("4") else "upper"))
-
-print("'!' => {}".format("lower" if islower("!") else "upper"))
