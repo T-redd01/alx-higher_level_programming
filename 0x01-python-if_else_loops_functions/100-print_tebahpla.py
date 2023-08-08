c80b0e90ed04de6ce385fb7f597077b64ebec432
T-redd01 <tyrilreddy1@gmail.com>
2023-08-08 22:42:48 +0200

    redid 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
-    if i % 2 == 0:
-        print(chr(i), end='')
-        continue
-    print(chr(i - 32), end='')
+    print("{:c}".format(i if i % 2 == 0 else i - 32), end='')
