decdac856fb44718ace45f1f29781a3014c4f3e9
T-redd01 <tyrilreddy1@gmail.com>
2023-08-13 04:58:23 -0700

    used isinstance

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,9 +5,7 @@ def print_reversed_list_integer(my_list=[]):
         Args:
             my_list: list of integers
     """
-    if type(my_list) != type([]):
-        return
-
-    my_list.reverse()
-    for i in my_list:
-        print("{:d}".format(i))
+    if isinstance(my_list, list):
+        my_list.reverse()
+        for i in my_list:
+            print("{:d}".format(i))
