f6750ee0ade551d1490f5c5cf663ef2414cbcf04
T-redd01 <tyrilreddy1@gmail.com>
2023-08-16 21:16:14 +0200

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not (isinstance(roman_string, str)) or roman_string == None:
+        return 0
+
     units = (("I", 1), ("II", 2), ("III", 3), ("IV", 4), ("V", 5), \
             ("VI", 6), ("VII", 7), ("VIII", 8), ("IX", 9))
     tens = (("X", 10), ("XX", 20), ("XXX", 30), ("XL", 40), ("L", 50), \
