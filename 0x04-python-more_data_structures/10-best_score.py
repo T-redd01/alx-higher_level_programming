11da1decaf20b44e039b0b1ac1fb50e005fcf30c
T-redd01 <tyrilreddy1@gmail.com>
2023-08-16 07:45:27 -0700

    removed debugging line

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,6 +5,5 @@ def best_score(a_dictionary):
         k = None
         for i, v in a_dictionary.items():
             if v > m:
-                print(v, m, i)
                 m, k = v, i
         return k
