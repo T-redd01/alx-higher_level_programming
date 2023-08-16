118d763ff7c5f959ac3dfc6d0d9eb67ee576c8d1
T-redd01 <tyrilreddy1@gmail.com>
2023-08-16 21:26:34 +0200

    pycodestyle

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not (isinstance(roman_string, str)) or roman_string == None:
+    if not (isinstance(roman_string, str)) or roman_string is None:
         return 0
 
-    units = (("I", 1), ("II", 2), ("III", 3), ("IV", 4), ("V", 5), \
+    units = (("I", 1), ("II", 2), ("III", 3), ("IV", 4), ("V", 5),
             ("VI", 6), ("VII", 7), ("VIII", 8), ("IX", 9))
     tens = (("X", 10), ("XX", 20), ("XXX", 30), ("XL", 40), ("L", 50),
             ("LX", 60), ("LXX", 70), ("LXXX", 80), ("XC", 90))
@@ -13,35 +13,35 @@ def roman_to_int(roman_string):
     thousands = (("M", 1000), ("MM", 2000), ("MMM", 3000))
     i = 0
     s = ""
-    l = len(roman_string)
+    ln = len(roman_string)
     num = 0
-    while i < l:
-        if roman_string[i] == 'M': # thousands
-            while i < l and roman_string[i] not in "CDLXIV":
+    while i < ln:
+        if roman_string[i] == 'M':
+            while i < ln and roman_string[i] not in "CDLXIV":
                 s += roman_string[i]
                 i += 1
             for r, n in thousands:
                 if s == r:
                     num += n
-        if i < l and roman_string[i] in "CDM": # hundreds
+        if i < ln and roman_string[i] in "CDM":
             s = ""
-            while i < l and roman_string[i] not in "XLVI":
+            while i < ln and roman_string[i] not in "XLVI":
                 s += roman_string[i]
                 i += 1
             for r, n in hundreds:
                 if s == r:
                     num += n
-        if i < l and roman_string[i] in "XLC": # tens
+        if i < ln and roman_string[i] in "XLC":
             s = ""
-            while i < l and roman_string[i] not in "IV":
+            while i < ln and roman_string[i] not in "IV":
                 s += roman_string[i]
                 i += 1
             for r, n in tens:
                 if s == r:
                     num += n
-        if i < l and roman_string[i] in "IV": # units
+        if i < ln and roman_string[i] in "IV":
             s = ""
-            while i < l:
+            while i < ln:
                 s += roman_string[i]
                 i += 1
             for r, n in units:
