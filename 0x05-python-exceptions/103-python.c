ada424ba2815cf254cc84cd2823851bcf7742d97
T-redd01 <tyrilreddy1@gmail.com>
2023-08-28 14:27:47 +0000

    trying

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,31 +1,78 @@
 #include <Python.h>
 
-void print_python_list(PyObject *p)
-{
-	printf("[*] Python list info\n");
-	if (!(PyList_Check(p)))
-	{
-		printf("[ERROR] Invalid List Object\n");
-		return;
-	}
-}
-
 void print_python_bytes(PyObject *p)
 {
+	PyBytesObject *byte_str = NULL;
+	Py_ssize_t i, size;
+
 	printf("[.] bytes object info\n");
 	if (!(PyBytes_Check(p)))
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
+
+	byte_str = (PyBytesObject *)p;
+	size = ((PyVarObject *)byte_str)->ob_size;
+	printf("  size: %zd\n", size);
+	printf("  trying string: ");
+	for (i = 0; i < size; i++)
+		printf("%c", byte_str->ob_sval[i]);
+	printf("\n");
+
+	if (size > 10)
+		size = 10;
+	else
+		size = size + 1;
+	
+	printf("  first %zd bytes: ", size);
+	for (i = 0; i < size; i++)
+		printf("%02x ", (unsigned char)(byte_str->ob_sval[i]));
+	printf("\n");
 }
 
 void print_python_float(PyObject *p)
 {
+	PyFloatObject *float_num;
+
 	printf("[.] float object info\n");
 	if (!(PyFloat_Check(p)))
 	{
 		printf("  [ERROR] Invalid List Object\n");
 		return;
+	}
+
+	float_num = (PyFloatObject *)p;
+	printf("  value: %f\n", float_num->ob_fval);
+}
+
+void print_python_list(PyObject *p)
+{
+	PyListObject *list = NULL;
+	PyObject *item = NULL;
+	Py_ssize_t i, size;
+	const char *name;
+
+	printf("[*] Python list info\n");
+	if (!(PyList_Check(p)))
+	{
+		printf("[ERROR] Invalid List Object\n");
+		return;
+	}
+
+	list = (PyListObject *)p;
+	size = ((PyVarObject *)list)->ob_size;
+	printf("[*] Size of the Python List = %zd\n", size);
+	printf("[*] Allocated = %zd\n", list->allocated);
+	for (i = 0; i < size; i++)
+	{
+		item = list->ob_item[i];
+		name = (item->ob_type)->tp_name;
+		printf("Element %zd: %s\n", i, name);
+		if (!(strcmp(name, "float")))
+			print_python_float(item);
+
+		if (!(strcmp(name, "bytes")))
+			print_python_bytes(item);
 	}
 }
--- 0x05-python-exceptions/test.py
+++ 0x05-python-exceptions/test.py
@@ -1,23 +1,41 @@
-#!/usr/bin/python3
-safe_function = __import__('101-safe_function').safe_function
+#!/usr/bin/python3 -u
 
+import ctypes
 
-def my_div(a, b):
-    return a / b
-
-result = safe_function(my_div, 10, 2)
-print("result of my_div: {}".format(result))
-
-result = safe_function(my_div, 10, 0)
-print("result of my_div: {}".format(result))
-
-
-def print_list(my_list, len):
-    i = 0
-    while i < len:
-        print(my_list[i])
-        i += 1
-    return len
-
-result = safe_function(print_list, [1, 2, 3, 4], 10)
-print("result of print_list: {}".format(result))
+lib = ctypes.CDLL('./libPython.so')
+lib.print_python_list.argtypes = [ctypes.py_object]
+lib.print_python_bytes.argtypes = [ctypes.py_object]
+lib.print_python_float.argtypes = [ctypes.py_object]
+s = b"Hello"
+lib.print_python_bytes(s);
+b = b'\xff\xf8\x00\x00\x00\x00\x00\x00';
+lib.print_python_bytes(b);
+b = b'What does the \'b\' character do in front of a string literal?';
+lib.print_python_bytes(b);
+l = [b'Hello', b'World']
+lib.print_python_list(l)
+del l[1]
+lib.print_python_list(l)
+l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], b"School", "Betty"]
+lib.print_python_list(l)
+l = []
+lib.print_python_list(l)
+l.append(0)
+lib.print_python_list(l)
+l.append(1)
+l.append(2)
+l.append(3)
+l.append(4)
+lib.print_python_list(l)
+l.pop()
+lib.print_python_list(l)
+l = ["School"]
+lib.print_python_list(l)
+lib.print_python_bytes(l);
+f = 3.14
+lib.print_python_float(f);
+l = [-1.0, -0.1, 0.0, 1.0, 3.14, 3.14159, 3.14159265, 3.141592653589793238462643383279502884197169399375105820974944592307816406286]
+print(l)
+lib.print_python_list(l);
+lib.print_python_float(l);
+lib.print_python_list(f);
