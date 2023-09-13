b5078f5916df8d3e76763d92b48e0f55593ec11d
T-redd01 <tyrilreddy1@gmail.com>
2023-09-13 03:26:44 -0700

    tryin

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,4 +3,11 @@
 
 
 def add_attribute(obj, name, value):
+    """sets an attribute
+
+    Args:
+        obj: class to set attr
+        name: name of new attr
+        value: value of attr
+    """
     setattr(obj, name, value)
