be214e542d119eadb16b8f821d15f7bc69cabd0c
T-redd01 <tyrilreddy1@gmail.com>
2023-09-13 06:57:35 -0700

    trying

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""trying to compute metrics"""
+import sys
+
+my_l = []
+try:
+    for line in sys.stdin:
+        words = line.split(" ")
+        new = 
+        print(words)
+except KeyboardInterrupt as e:
+    print("caught interrupt")
--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -13,3 +13,21 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
+
+    new = [[1]]
+    foo = new[0]
+    tmp = []
+
+    for i in range(n - 1):
+        tmp = []
+        for j in range(len(foo)):
+            if j == 0:
+                tmp.append(foo[j])
+
+            if j + 1 != len(foo):
+                tmp.append(foo[j] + foo[j + 1])
+            else:
+                tmp.append(foo[j])
+        new.append(tmp)
+        foo = tmp
+    return new
