5799251186a0c6d60aa9f6f1b4f04c585fdf3bec
T-redd01 <tyrilreddy1@gmail.com>
2023-09-13 07:40:03 -0700

    101 is tough

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -2,11 +2,23 @@
 """trying to compute metrics"""
 import sys
 
+num = 0
+counter = 0
 my_l = []
 try:
     for line in sys.stdin:
+        num += len(line)
+        counter += 1
         words = line.split(" ")
-        new = 
-        print(words)
+        new = [words[7], words[8]]
+        my_l.append(new)
+        my_l.sort()
+        if counter == 10:
+            counter = 0
+            print(f"File size: {num:d}")
+            for j in my_l:
+                print(f"{j[0]}: {j[1]}")
 except KeyboardInterrupt as e:
-    print("caught interrupt")
+    print(f"File size: {num:d}")
+    for i in my_l:
+        print(f"{i[0]}: {i[1]}")
