d60625a87b29b85bf0025e19ccfd95d028904e4b
T-redd01 <tyrilreddy1@gmail.com>
2023-10-19 01:19:00 -0700

    trying

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,4 @@
 -- create a table with fields
 -- field if table cannot be null
 
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
