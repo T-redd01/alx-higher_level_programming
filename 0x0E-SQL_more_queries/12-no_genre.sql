adac8dd3e898ecffc350932140ef2c4e08d00e66
T-redd01 <tyrilreddy1@gmail.com>
2023-10-19 06:30:43 -0700

    trying

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -5,5 +5,5 @@ SELECT title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN hbtn_0d_tvshows.tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id IS NULL
+WHERE tv_show_genres.show_id IS NULL
 ORDER BY tv_shows.title ASC;
--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -8,7 +8,7 @@ ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_show_genres.show_id =
 	(SELECT DISTINCT show_id
 		FROM tv_show_genres
-		INNER JOIN tv_shows
-	 ON tv_shows.id = tv_show_genres.show_id
-		WHERE tv_shows.title = 'Dexter')
+	 INNER JOIN tv_shows
+		ON tv_shows.id = tv_show_genres.show_id
+	 WHERE tv_shows.title = 'Dexter')
 ORDER BY name;
