a81441939834e8116f15777b53b823d3224e4dca
T-redd01 <tyrilreddy1@gmail.com>
2023-10-19 05:42:33 -0700

    :(

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -6,9 +6,10 @@ FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_show_genres.show_id = (SELECT DISTINCT show_id
-	FROM tv_show_genres
-	INNER JOIN tv_shows
-	ON tv_shows.id = tv_show_genres.show_id
-	WHERE tv_shows.title = 'Dexter')
+								FROM tv_show_genres
+								INNER JOIN tv_shows
+								ON tv_shows.id = tv_show_genres.show_id
+								WHERE tv_shows.title = 'Dexter'
+)
 ORDER BY name
 ;
