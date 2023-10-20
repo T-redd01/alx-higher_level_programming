5dd14a1a4076aeb44094080d70801076aaf9402a
T-redd01 <tyrilreddy1@gmail.com>
2023-10-19 20:02:45 -0700

    trying

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -5,6 +5,6 @@ SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres
-ON tv_genre.id = tv_show_genre.genre_id
+LEFT JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY tv_shows.title, tv_genres.name ASC;
