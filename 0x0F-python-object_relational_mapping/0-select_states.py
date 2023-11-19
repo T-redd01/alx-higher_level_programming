59151da762ad506a3a552fcef799acbf9cd99505
T-redd01 <tyrilreddy1@gmail.com>
2023-11-19 17:14:36 +0200

    typo

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,12 @@
-#!/usr/bin/python3
+#!/home/otto/Documents/ORM-venv/venv/bin/python3
+"""#!/usr/bin/python3"""
 """my first script with python db connector"""
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306 user=sys.argv[1],
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
