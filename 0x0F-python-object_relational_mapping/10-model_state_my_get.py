134c3a232f69eace33568d3c1e5323d3e152bb52
T-redd01 <tyrilreddy1@gmail.com>
2023-11-20 03:18:24 -0800

    :(

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -3,6 +3,7 @@
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from sqlalchemy import asc
 import sys
 
 
@@ -12,8 +13,8 @@ if __name__ == "__main__":
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = Session(engine)
-    state = session.query(State).order_by(State.id).first()
-    if state:
-        print(f"{state.id}: {state.name}")
-    else:
-        print("")
+    states = session.query(State).where(State.name==sys.argv[4])
+    for state in states:
+        print(f"{state.id}")
+    if not states:
+        print("Not found")
